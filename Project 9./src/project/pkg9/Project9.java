2a6d34ba665cc530d6fb822e47a9e01f71e50a99
shelbaltz <shelbaltz@192.168.2.15>
2016-10-26 23:37:05 -0600

    9

---
+++ Project 9./src/project/pkg9/Project9.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.pkg9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author shelbaltz
+ */
+public class Project9 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner reader = new Scanner(System.in);
+        double n = reader.nextInt();
+        System.out.println("The square root is " + Math.sqrt(n));
+        
+        //project 10
+    }
+    
+}
